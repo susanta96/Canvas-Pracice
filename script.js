2701d9e908c5b28df4b641d3dabbe6c5cf25014f
Susanta Chakraborty <svchost96@gmail.com>
2024-01-07 21:52:23 +0530

    fix: created animated different balls and use requestAnimationFrame to do it

--- script.js
+++ script.js
@@ -3,11 +3,14 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 const ctx = canvas.getContext("2d");
+const balls = [];
 
 class Ball {
   constructor(x, y) {
     this.x = x;
     this.y = y;
+    this.xVelocity = (Math.random() - 0.5) * 10;
+    this.yVelocity = (Math.random() - 0.5) * 10;
     this.size = Math.random() * 30 + 10;
     this.color = `hsl(${Math.random() * 360}, ${Math.random() * 100}%, ${
       Math.random() * 100
@@ -19,9 +22,31 @@ class Ball {
     ctx.arc(this.x, this.y, 30, 0, Math.PI * 2);
     ctx.fill();
   }
+  update() {
+    this.x += this.xVelocity;
+    this.y += this.yVelocity;
+    // if (this.x - this.size < 0 || this.x + this.size > canvas.width) {
+    //   this.xVelocity *= -1;
+    // }
+    // if (this.y - this.size < 0 || this.y + this.size > canvas.height) {
+    //   this.yVelocity *= -1;
+    // }
+  }
 }
+
+function animate() {
+  ctx.fillStyle = "#f2f2f2";
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  balls.forEach((ball) => {
+    ball.update();
+    ball.draw();
+  });
+  requestAnimationFrame(animate);
+}
+
+animate();
 
 canvas.addEventListener("click", (e) => {
   const ball = new Ball(e.clientX, e.clientY);
-  ball.draw();
+  balls.push(ball);
 });
